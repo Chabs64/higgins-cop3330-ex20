a300dae721ab2faab429c9249f2b8e5b452cbe7d
chad <chadhiggins6@gmail.com>
2021-06-06 21:04:54 -0400

    exercise 20

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -32,10 +32,58 @@ Ensure that all money is rounded up to the nearest cent.
 Use a single output statement at the end of the program to display the program results.
  */
 
-public class App 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("What is the order amount? ");
+        BigDecimal amount = new BigDecimal(in.nextLine());
+        System.out.println("What state do you live in? ");
+        String state = in.nextLine();
+
+        double taxRate = 0.00;
+        BigDecimal taxAmount = new BigDecimal("0");
+        BigDecimal total = new BigDecimal("0");
+
+        if(state.equals("Wisconsin"))
+        {
+            System.out.println("What county do you live in?");
+            String county = in.nextLine();
+
+            if(county.equals("Eau Claire"))
+            {
+                taxRate += 0.005;
+            }
+            else if(county.equals("Dunn"))
+            {
+                taxRate += 0.004;
+            }
+
+            taxRate += 0.05;
+            taxAmount = amount.multiply(new BigDecimal(taxRate));
+            total = amount.add(taxAmount);
+            System.out.println("The tax is $"+taxAmount.setScale(2,RoundingMode.HALF_UP)+".\n" +
+                    "The total is $"+total.setScale(2,RoundingMode.HALF_UP)+".");
+        }
+        else if(state.equals("Illinois"))
+        {
+            taxRate += 0.08;
+            taxAmount = amount.multiply(new BigDecimal(taxRate));
+            total = amount.add(taxAmount);
+            System.out.println("The tax is $"+taxAmount.setScale(2,RoundingMode.HALF_UP)+".\n" +
+                    "The total is $"+total.setScale(2,RoundingMode.HALF_UP)+".");
+        }
+        else
+        {
+            taxAmount = amount.multiply(new BigDecimal(taxRate));
+            total = amount.add(taxAmount);
+            System.out.println("The total is $"+total.setScale(2,RoundingMode.HALF_UP)+".");
+        }
     }
 }
